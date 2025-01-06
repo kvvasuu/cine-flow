d6e5121a827180b613fefbe223748b65720ac577
kvvasu <lukaszkwas96@gmail.com>
2025-01-06 21:19:29 +0100

    .

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,17 +2,19 @@ import ProfileIMG from "./assets/images/ProfileIMG.png";
 import SearchIcon from "./assets/icons/Search.svg";
 import GiftIcon from "./assets/icons/GiftBox.svg";
 import BellIcon from "./assets/icons/NotificationBell.svg";
+import Logo from "./assets/images/logo.png";
 
 function Navbar() {
   return (
     <header className="w-full h-20 flex items-center justify-between px-14">
       <div id="header-left" className="flex items-center gap-12">
-        <h1
+        <img
           id="logo"
-          className="text-ruby font-clash font-semibold text-3xl select-none"
-        >
-          CINEFLOW
-        </h1>
+          src={Logo}
+          alt="Cineflow"
+          className="h-8 select-none drop-shadow-[0_0_3px_rgba(185,9,11,0.5)]"
+        />
+
         <nav id="navbar">
           <ol className="flex items-center justify-center gap-5 text-sm text-neutral-200 cursor-pointer">
             <li className="font-bold text-neutral-50">Home</li>
