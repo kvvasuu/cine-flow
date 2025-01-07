2412b682f2098fd5b0e329529b02fe14e35268c1
kvvasu <lukaszkwas96@gmail.com>
2025-01-07 22:09:42 +0100

    .

--- src/components/SliderListElement.tsx
+++ src/components/SliderListElement.tsx
@@ -5,13 +5,13 @@ interface Props {
 
 function SliderList({ name, img }: Props) {
   return (
-    <div className="w-[284px] h-40 flex flex-col relative shrink-0 overflow-hidden rounded-sm group cursor-pointer snap-start shadow">
+    <div className="w-[284px] h-40 flex flex-col relative shrink-0 overflow-hidden rounded-sm group cursor-pointer snap-start">
       <img
         className="w-full h-full absolute top-0 left-0"
         src={img}
         alt={name}
       />
-      <div className="w-full h-full z-10 bg-gradient flex flex-col justify-end p-2 transition-all opacity-0 group-hover:opacity-100">
+      <div className="w-full h-full absolute bottom-0 left-0 z-10 bg-gradient flex flex-col justify-end p-2 transition-all opacity-0 group-hover:opacity-100 rounded-sm">
         <h3 className="font-bold text-2xl text-neutral-100 select-none">
           {name}
         </h3>
